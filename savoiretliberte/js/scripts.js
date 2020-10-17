bdbf49738f0c802b6b6cbaac67e9edfe16adb21e
hafsa-k <43752595+hafsa-k@users.noreply.github.com>
2020-10-17 15:47:47 +0200

    Add files via upload

---
+++ savoiretliberte/js/scripts.js
@@ -0,0 +1,16 @@
+
+
+
+$( document ).ready(function(){
+ 
+$(".navbar-nav").hidden();
+
+$(".navbar-toggler").clip(function() {
+	$(".navbar-nav").show("1000");
+})
+
+        console.log( "hello" );
+
+        
+
+});
